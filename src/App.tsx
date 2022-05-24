d71983d5e654a686fa3f982a4b91702bacf710e0
Tsawlen <78802953+Tsawlen@users.noreply.github.com>
2022-05-24 09:49:19 +0200

    TestNavBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.scss'
 import concert from './ressources/concert.jpg';
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import Menubar from './components/snippets/Menubar';
+
 
 function App() {
 
   return (
     <div className='main'>
-      <Parallax pages={2}>
+      <Parallax pages={3}>
         {/* <ParallaxLayer speed={1}>
             <h2>Welcome to my website</h2>
         </ParallaxLayer>
@@ -30,14 +32,20 @@ function App() {
           speed={2.2} 
           factor={1}
         >
+          
           <h1 className='center'>Instrumentenverleih</h1>
+          
         </ParallaxLayer>
 
         <ParallaxLayer
           offset={1}
-          speed={1} 
+          speed={200} 
           factor={1}
-        />
+          sticky={{start: 1, end: 3}}
+        >
+          <Menubar></Menubar>
+        </ParallaxLayer>
+
 
       </Parallax>
     </div>
---
+++ src/components/snippets/Menubar.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import { IconButton, Toolbar, Typography } from '@mui/material';
+
+const pages = ['Instrumente', 'Reparatur', 'Marktplatz']
+
+const Menubar = () => {
+
+    return(
+        <AppBar position="relative">
+            <Toolbar variant="regular" className="toolbar">
+                <Typography 
+                    variant = "h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    className="toolbarText"
+                >
+                    Instrumente
+                </Typography>
+                
+                <Typography
+                    variant = "h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    className="toolbarText"
+                >
+                    Reperatur
+                </Typography>
+                <Typography
+                    variant = "h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    className="toolbarText"
+                >
+                    Marktplatz
+                </Typography>
+
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Menubar;
+
