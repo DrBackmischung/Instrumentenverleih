43a131a6cea7dc8a87a478e933ce602b35382828
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-05-31 11:43:10 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,21 @@
 import './App.scss'
-import concert from './ressources/guitar5.jpg';
-
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import Home from './pages/Home';
+import NavigationBar from './components/NavigationBar';
+import { Route, Router, Routes } from 'react-router-dom';
+import FooterBar from './components/FooterBar';
+import Ausleihe from './pages/Ausleihe';
 
 function App() {
 
   return (
-    <div className='main'>
-      <Parallax pages={2}>
-        <ParallaxLayer
-          offset={0}
-          speed={1}
-          factor={2.3}
-          style={{
-            backgroundImage: `url(${concert})`,
-            backgroundSize: 'cover',
-            backgroundColor: 'black'
-          }}
-        />
-
-        <ParallaxLayer
-          offset={0}
-          speed={0.05} 
-          factor={1}
-        >
-          
-          <h1 className='home-header'>Instrumentenverleih</h1>
-          <h4 className='home-swipe'>Swipe up!</h4>
-          <h4 className='home-arrow'>^</h4>
-          
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={0.7}
-          speed={1} 
-          factor={1.3}
-          style={{
-            backgroundImage: 'linear-gradient(rgba(49,43,36,0), black 80%)'
-          }}
-        >
-        </ParallaxLayer>
-
-
-        <ParallaxLayer
-          offset={1}
-          speed={0.6} 
-          factor={1}
-          style={{
-            backgroundColor: 'black'
-          }}
-        >
-          <Home />
-        </ParallaxLayer>
-
-      </Parallax>
-    </div>
+    <>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ausleihe" element={<><NavigationBar/><Ausleihe /><FooterBar/></>} />
+          <Route path="/marktplatz" element={<><NavigationBar/><Ausleihe /><FooterBar/></>} />
+          <Route path="/serviceportal" element={<><NavigationBar/><Ausleihe /><FooterBar/></>} />
+      </Routes>
+    </>
   )
 }
 
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,48 +1,93 @@
 import * as React from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
-import { IconButton, Toolbar, Typography } from '@mui/material';
+import { Box, Button, Container, IconButton, Link, Menu, MenuProps, Toolbar, Typography } from '@mui/material';
 import './styles/NavigationBar.scss';
+import styled from '@emotion/styled';
 
-const pages = ['Instrumente', 'Reparatur', 'Marktplatz']
+function NavigationBar() {
 
-const NavigationBar = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const [pages, setPages] = useState([
+        { name: "Ausleihe", link: "/ausleihe" },
+        { name: "Marktplatz", link: "/marktplatz" },
+        { name: "Serviceportal", link: "/serviceportal" },
+    ]);
+  
+    const StyledMenu = styled((props: MenuProps) => (
+      <Menu
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        {...props}
+      />
+    ))(({theme})=>({
+      '& .MuiPaper-root': {
+        borderRadius: 6,
+        minWidth:180,
+      },
+      '& .MuiMenu-list': {
+        backgroundColor: '#ba8434',
+        padding: '4px 0',
+      },
+    }));
 
     return(
-        <AppBar position="relative">
-            <Toolbar variant="regular" className="toolbar">
-                <Typography 
-                    variant = "h6"
-                    noWrap
-                    component="a"
-                    href="/"
-                    className="toolbarText"
-                >
-                    Instrumente
-                </Typography>
-                
-                <Typography
-                    variant = "h6"
-                    noWrap
-                    component="a"
-                    href="/"
-                    className="toolbarText"
-                >
-                    Reperatur
-                </Typography>
-                <Typography
-                    variant = "h6"
-                    noWrap
-                    component="a"
-                    href="/"
-                    className="toolbarText"
-                >
-                    Marktplatz
-                </Typography>
-
+        <AppBar
+          style={{ backgroundColor: "#393E41", opacity: 1 }}
+          position="sticky"
+        >
+          <Container fixed>
+            <Toolbar>
+              <IconButton
+                sx={{ height: "40px", width: "40px" }}
+                size="large"
+                edge="start"
+                component={Link}
+                href="/"
+              >
+                <img
+                  src="https://raw.githubusercontent.com/DrBackmischung/Kino-Dokumentation/main/KV.png"
+                  alt="Kinovation Logo"
+                  height={40} />
+              </IconButton>
+              <Box
+                sx={{
+                  justifyContent: "space-evenly",
+                  flexGrow: 1,
+                  display: { xs: "flex" },
+                }}
+              >
+                {pages.map((page) => (
+                  <Button
+                    style={{ backgroundColor: "#ba8434", opacity: 0.95 }}
+                    key={page.name}
+                    sx={{ my: 2, color: "black", display: "block" }}
+                    variant="contained"
+                    component={Link}
+                    href={page.link}
+                  >
+                    {page.name}
+                  </Button>
+                ))}
+              </Box>
             </Toolbar>
+          </Container>
         </AppBar>
     );
 }
 
 export default NavigationBar;
-
