f1e4019b40c0f74145d507ca0a40216172b73898
Tsawlen <78802953+Tsawlen@users.noreply.github.com>
2022-06-04 13:27:17 +0200

    V1

--- src/pages/VerstaerkerDetail.tsx
+++ src/pages/VerstaerkerDetail.tsx
@@ -8,6 +8,7 @@ import Gehäuse from '../ressources/Gehäuse.svg';
 import Kontrollpanel from '../ressources/Kontrollpanel.svg';
 import Lautsprecher from '../ressources/Lautsprecher.svg';
 import Verstaerker from '../ressources/Verstärker.svg';
+import Uebersicht from '../ressources/Uebersicht.jpg';
 
 
 
@@ -58,7 +59,24 @@ function VerstaerkerDetail(){
                     <Typography className='parallaxText'>Dein Sound hat einfach nicht den Wums, welchen du haben möchtest? Deine Sounds zerfetzen nicht deine Ohren? Dann hast du einach den falschen Verstärker! Leihe dir unseren Marshall Verstärker der Oberklasse!</Typography>
                 </Grid>
 
-                <Grid item xs={12} sm={12} md={12} lg={6} order={{xs:4, sm:4, md:4, lg: 3}}>
+                <Grid item xs={12} sm={12} md={12} lg={12} order={{xs:3, sm:3, md:3, lg: 3}} sx={{backgroundImage: `url(${Uebersicht})`, backgroundSize: "cover"}} >
+                    
+                    <div className="Uebersicht">
+
+                        <Typography variant="h3" sx={{fontWeight: 'bold'}}>Alles auf einen Blick</Typography>
+                        <ul >
+                            <li>Höhen einstellbar</li>
+                            <li>Mitten einstellbar</li>
+                            <li>Bass einstellbar</li>
+                            <li>2x 12" Treiber</li>
+                            <li>50 Watt</li>
+                        </ul>
+
+                    </div>
+
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6} order={{xs:5, sm:5, md:5, lg: 4}} sx={{backgroundColor: 'black'}}>
 
                     <picture>
                         <img src={Kontrollpanel} alt="Kontroll-Panel" srcSet={Kontrollpanel}/>
@@ -66,13 +84,13 @@ function VerstaerkerDetail(){
 
                 </Grid>
 
-                <Grid item xs={12} sm={12} md={12} lg={6} order={{xs:3, sm:3, md:3, lg: 4}}>
+                <Grid item xs={12} sm={12} md={12} lg={6} order={{xs:4, sm:4, md:4, lg: 5}} sx={{backgroundColor: 'black'}}> 
                     <Typography variant="h3" className='parallaxHeader' >Alles unter Kontrolle</Typography>
                     <Typography className='parallaxText'>Möchtest du mit deinen Bässen ein Erdberben auslösen oder doch nur ein leichtest Wummern? Möchtest du ein Mikrophon anschließen und mit deinem Gesang deine Zuhörer begeistern oder legst du doch mehr Wert auf die Höhen?</Typography>
                     <Typography className='parallaxText'>Das Kontroll-Panel dieses Verstärkers lässt keine Wünsche offen!</Typography>
                 </Grid>
                 
-                <Grid item xs={12} sm={12} md={12} lg={6} order={{xs:5, sm:5, md:5, lg: 5}} >
+                <Grid item xs={12} sm={12} md={12} lg={6} order={{xs:6, sm:6, md:6, lg: 6}} sx={{backgroundColor: 'black'}}>
 
                     <picture>
                         <img src={Lautsprecher} alt="Lautsprecher" srcSet={Lautsprecher}/>
@@ -80,7 +98,7 @@ function VerstaerkerDetail(){
 
                 </Grid>
 
-                <Grid item xs={12} sm={12} md={12} lg={6} order={{xs:4, sm:4, md:4, lg: 4}}>
+                <Grid item xs={12} sm={12} md={12} lg={6} order={{xs:5, sm:5, md:5, lg: 5}} sx={{backgroundColor: 'black'}}>
                     <Typography variant="h3" className='parallaxHeader' >Satte Klänge</Typography>
                     <Typography className='parallaxText'>Egal wie laut du dieses Gerät aufdrehst die Klänge bleiben immer sauber, dank der beiden verbauten 12 Zoll Treibern mit jeweils 50 Watt! Diese Monster können nicht überfordert werden!</Typography>
                 </Grid>
